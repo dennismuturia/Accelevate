79abd36cb962e14573611cc8743eeb7e505dd1c7
dennismuturia <dmuturia2922@gmail.com>
2018-07-25 17:13:01 +0300

    Inserted the navigation bar. Was having difficulties in making tabs appear on the right. But i did it.

--- Accelevate/settings.py
+++ Accelevate/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'index',
-    'bootstrap3',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
